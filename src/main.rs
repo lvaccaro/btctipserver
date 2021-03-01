b553492777f2f0e91e87e264870cf062b6c18492
Luca Vaccaro <me@lvaccaro.com>
2021-03-01 19:55:11 +0100

    check address: pass client and update status label

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ use bdk::{Wallet};
 use bdk::bitcoin::Address;
 
 use simple_server::{Method, Server, StatusCode};
-use bdk::electrum_client::{Client, ElectrumApi, ListUnspentRes};
+use bdk::electrum_client::{Client, ElectrumApi, ListUnspentRes, Error};
 
 fn prepare_home_dir() -> PathBuf {
     let mut dir = PathBuf::new();
@@ -60,8 +60,7 @@ fn new_address() -> Result<Address, bdk::Error> {
     Ok(addr)
 }
 
-fn check_address(addr: String, from_height: Option<usize>) -> Result<Vec<ListUnspentRes>, bdk::Error> {
-
+fn client() -> Result<Client, Error> {
     let conf = Ini::load_from_file("config.ini").unwrap();
     let section_bdk = conf.section(Some("BDK")).unwrap();
     let network = section_bdk.get("network").unwrap();
@@ -70,8 +69,10 @@ fn check_address(addr: String, from_height: Option<usize>) -> Result<Vec<ListUns
         bdk::bitcoin::Network::Testnet => { "ssl://electrum.blockstream.info:60002"}
         _ => { "" }
     };
+    Client::new(url)
+}
 
-    let client = Client::new(url).unwrap();
+fn check_address(client: &Client, addr: String, from_height: Option<usize>) -> Result<Vec<ListUnspentRes>, bdk::Error> {
 
     let monitor_script = Address::from_str(addr.as_str())
         .unwrap()
@@ -89,13 +90,19 @@ fn check_address(addr: String, from_height: Option<usize>) -> Result<Vec<ListUns
 }
 
 fn html(address: String) -> Result<String, std::io::Error> {
-    let list = check_address(address.as_str().to_string(), Option::from(0)).unwrap();
-    let amount: u64 = list.iter().map(|x| x.value).sum();
+    let client = client().unwrap();
+    let list = check_address(&client, address.as_str().to_string(), Option::from(0)).unwrap();
 
-    let mut status = "No onchain tx found yet".to_string();
-    if amount > 0 {
-        status = format!("Received {} sat", amount.to_string());
-    }
+    let status = match list.last() {
+        None => { "No onchain tx found yet".to_string() }
+        Some(unspent) => {
+            let mut location = "in mempool".to_string();
+            if unspent.height > 0 {
+                location = format!("at {}", unspent.height.to_string());
+            }
+            format!("Received {} sat {}", unspent.value, location)
+        }
+    };
 
     let template = fs::read_to_string("assets/index.html").unwrap();
     let link = format!("/bitcoin/?{}", address);
@@ -153,7 +160,8 @@ fn main() {
                 let height = query.next().unwrap();
                 let h: usize = height.parse::<usize>().unwrap();
 
-                let list = check_address(addr.to_string(), Option::from(h));
+                let client = client().unwrap();
+                let list = check_address(&client, addr.to_string(), Option::from(h));
                 return match list {
                     Ok(list) => {
                         println!("addr {} height {}", addr, h);
