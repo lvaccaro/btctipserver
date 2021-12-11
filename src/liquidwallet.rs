d6d3d660ed7772d10c8a17e92cd80f7da46e6fe9
Luca Vaccaro <me@lvaccaro.com>
2021-12-11 16:48:52 +0100

    liquid: implement liquid wallet functions
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- src/liquidwallet.rs
+++ src/liquidwallet.rs
@@ -1,14 +1,17 @@
 use crate::config::LiquidOpts;
 use crate::error::Error;
+use crate::server::gen_err;
 use crate::wallet::Wallet;
 
+use std::collections::HashMap;
 use std::str::FromStr;
 
-use edk::bdk::electrum_client::{Client};
+use edk::bdk::electrum_client::Client;
 
 use bdk::sled::{self, Tree};
 use edk::miniscript::elements::secp256k1_zkp;
 use edk::miniscript::elements::slip77::MasterBlindingKey;
+use edk::miniscript::elements::Address;
 use edk::miniscript::{Descriptor, DescriptorPublicKey};
 use hex;
 
@@ -23,7 +26,6 @@ impl LiquidWallet {
         let tree = database.open_tree(&opts.wallet)?;
 
         // setup electrum blockchain client
-        let electrum_opts = opts.electrum_opts.clone();
         let client = Client::new(&opts.electrum_opts.electrum).unwrap();
 
         // setup keys variables
@@ -46,23 +48,39 @@ impl LiquidWallet {
 }
 
 impl Wallet for LiquidWallet {
-    fn last_unused_address(&self) -> Result<String, bdk::Error> {
-        Ok("".to_string())
+    fn last_unused_address(&self) -> Result<String, simple_server::Error> {
+        let address = self.wallet.get_new_address().map_err(|_| gen_err())?;
+        Ok(address.to_string())
     }
 
     fn is_my_address(&self, addr: &str) -> Result<bool, simple_server::Error> {
-        Ok(true)
+        let address = Address::from_str(addr).map_err(|_| gen_err())?;
+        self.wallet.is_mine_address(&address).map_err(|_| gen_err())
     }
 
-    fn check_address(
+    fn balance_address(
         &self,
         addr: &str,
         from_height: Option<usize>,
-    ) -> Result<Vec<bdk::electrum_client::ListUnspentRes>, simple_server::Error> {
-        Ok(vec![])
+    ) -> Result<HashMap<String, u64>, simple_server::Error> {
+        let addr = Address::from_str(addr).map_err(|_| gen_err())?;
+        let mut balances = HashMap::new();
+        for unblind in self
+            .wallet
+            .balance_addresses(vec![addr])
+            .map_err(|_| gen_err())?
+            .unblinds
+        {
+            let tx_out = unblind.1;
+            *balances.entry(tx_out.asset.to_string()).or_insert(0) += tx_out.value;
+        }
+        Ok(balances)
     }
 
     fn network(&self) -> Result<String, bdk::Error> {
-        Ok("liquid".to_string())
+        match self.wallet.network() {
+            &edk::miniscript::elements::AddressParams::LIQUID => Ok("liquid".to_string()),
+            _ => Ok("elements".to_string()),
+        }
     }
 }
